ddb6b53e2570be3f160eb29b8c71181b2a9af47a
bielfaria2003@gmail.com <bielfaria2003@gmail.com>
2025-12-28 23:54:42 -0300

    chore(app): atualizar configuração da aplicação
    Usa imagem única (Imgur) em picture, atualiza email para bielfaria2003@gmail.com e remove meetingLink em app/app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,11 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      dark: 'https://i.imgur.com/5prWFBk.jpeg',
+      light: 'https://i.imgur.com/5prWFBk.jpeg',
       alt: 'My profile picture'
     },
-    meetingLink: 'https://cal.com/',
-    email: 'ui-pro@nuxt.com',
+    email: 'bielfaria2003@gmail.com',
     available: true
   },
   ui: {
