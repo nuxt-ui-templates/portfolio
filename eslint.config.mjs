f9a7590d48a68ac0dc51275e739bdae5908953c8
Benjamin Canac <canacb1@gmail.com>
2026-09-15 11:38:34 +0200

    chore: lint dynamic `:class` values too

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default withNuxt(
       'better-tailwindcss': {
         entryPoint: 'app/assets/css/main.css',
         attributes: [
-          '^(v-bind:|:)?class$',
+          ['^(v-bind:|:)?class$', [{ match: 'strings' }, { match: 'objectKeys' }]],
           ['^(v-bind:|:)?ui$', [{ match: 'objectValues' }]]
         ]
       }
